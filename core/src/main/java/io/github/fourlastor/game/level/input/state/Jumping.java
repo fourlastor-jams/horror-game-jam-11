bde93cb995744f2ce3de095d01021249f8f22d0d
Daniele Conti <fourlastor@users.noreply.github.com>
2023-02-08 18:15:26 +0000

    Avoid creating vector

--- core/src/main/java/io/github/fourlastor/game/level/input/state/Jumping.java
+++ core/src/main/java/io/github/fourlastor/game/level/input/state/Jumping.java
@@ -2,13 +2,13 @@ package io.github.fourlastor.game.level.input.state;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import io.github.fourlastor.game.level.GameConfig;
 import io.github.fourlastor.game.level.component.AnimatedComponent;
 import io.github.fourlastor.game.level.component.BodyComponent;
 import io.github.fourlastor.game.level.component.InputComponent;
 import io.github.fourlastor.game.level.component.PlayerComponent;
+
 import javax.inject.Inject;
 
 public class Jumping extends CharacterState {
@@ -57,15 +57,14 @@ public class Jumping extends CharacterState {
 
         //gravity is given per second, but we want time step values here
         float t = 1 / 60.0f;
-        Vector2 stepGravity = config.gravity.cpy().scl(t * t); // m/s/s
+        float stepGravity = config.gravity.y  * t * t; // m/s/s
 
         //quadratic equation setup (ax² + bx + c = 0)
-        float a = 0.5f / stepGravity.y;
+        float a = 0.5f / stepGravity;
         float b = 0.5f;
         @SuppressWarnings("UnnecessaryLocalVariable") // names
         float c = desiredHeight;
 
-        //check both possible solutions
         float quadraticSolution = Math.abs((float) (( -b - Math.sqrt( b*b - 4*a*c ) ) / (2*a)));
 
         //convert answer back to seconds
