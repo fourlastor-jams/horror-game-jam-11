179cde5c719eadec292842a505bd44b4f4a8fdfb
Daniele Conti <fourlastor@users.noreply.github.com>
2023-02-10 14:57:31 +0000

    Reduce player max jump height

--- core/src/main/java/io/github/fourlastor/game/level/di/LevelModule.java
+++ core/src/main/java/io/github/fourlastor/game/level/di/LevelModule.java
@@ -92,7 +92,7 @@ public class LevelModule {
         return new GameConfig(
                 new GameConfig.Display(16f, 9f, 1f / 16f),
                 new GameConfig.Physics(new Vector2(0f, -10f)),
-                new GameConfig.Player(8f, 0.1f, 1f, 4f, 0.2f, new Vector2(0f, -20f)),
+                new GameConfig.Player(8f, 0.1f, 1f, 2.5f, 0.2f, new Vector2(0f, -20f)),
                 new GameConfig.Entities(0.7f));
     }
 }
