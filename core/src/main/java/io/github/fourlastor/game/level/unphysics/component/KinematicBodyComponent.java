eaecc56e234cad98b8959d74b8ef6ec6eb7b91b6
Daniele Conti <fourlastor@users.noreply.github.com>
2023-02-15 20:27:34 +0000

    Use object set for sensors list

--- core/src/main/java/io/github/fourlastor/game/level/unphysics/component/KinematicBodyComponent.java
+++ core/src/main/java/io/github/fourlastor/game/level/unphysics/component/KinematicBodyComponent.java
@@ -3,13 +3,11 @@ package io.github.fourlastor.game.level.unphysics.component;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.GridPoint2;
-
-import java.util.HashSet;
-import java.util.Set;
+import com.badlogic.gdx.utils.ObjectSet;
 
 public class KinematicBodyComponent implements Component {
     public final GridPoint2 collision = new GridPoint2();
     public final GridPoint2 touching = new GridPoint2();
 
-    public final Set<Entity> sensors = new HashSet<>();
+    public final ObjectSet<Entity> sensors = new ObjectSet<>(10);
 }
