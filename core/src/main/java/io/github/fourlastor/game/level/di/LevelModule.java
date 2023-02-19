29334361f1a27fc3b7fccb38a9adcfa638731c5c
Daniele Conti <fourlastor@users.noreply.github.com>
2023-02-19 23:22:48 +0000

    Hide debug system

--- core/src/main/java/io/github/fourlastor/game/level/di/LevelModule.java
+++ core/src/main/java/io/github/fourlastor/game/level/di/LevelModule.java
@@ -56,7 +56,7 @@ public class LevelModule {
         engine.addSystem(cameraMovementSystem);
         engine.addSystem(clearScreenSystem);
         engine.addSystem(stageSystem);
-        engine.addSystem(transformDebugSystem);
+        //        engine.addSystem(transformDebugSystem);
         return engine;
     }
 
@@ -93,7 +93,7 @@ public class LevelModule {
     @Provides
     public GameConfig gameConfig() {
         return new GameConfig(
-                new GameConfig.Display(256f, 144f, 1f),
+                new GameConfig.Display(256f * 5, 144f * 5, 1f),
                 new GameConfig.Physics(new Vector2(0f, -216)),
                 new GameConfig.Player(96f, 0.1f, 16f, 48f, 144f, 0.2f, 2f, 24f),
                 new GameConfig.Entities(0.7f));
